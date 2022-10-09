0c9c24235d5fcd51cad93633e905f8f88776f8cd
BhavyBansal24 <bhavy.209309023@muj.manipal.edu>
2022-10-09 22:09:22 +0530

    take a shot addon

--- app.py
+++ app.py
@@ -8,8 +8,22 @@ le = LabelEncoder()
 st.sidebar.title("Tomato Plant Disease Detector")
 
 selectbox = st.sidebar.selectbox(
-    'Select the type of input', ('None','Upload Image', 'Live Camera'))
+    'Select the type of input', ('None','Upload Image', 'Take A Shot', 'Live Camera (Experimental)'))
 
+# Take A Shot
+if selectbox == 'Take A Shot':
+    Uploaded_file = st.camera_input('Take A Shot')
+    if Uploaded_file is not None:
+        file_bytes = np.asarray(
+            bytearray(Uploaded_file.read()), dtype=np.uint8)
+        image = cv2.imdecode(file_bytes, 1)
+        frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = helper.preprocess(frame)
+        # st.image(image, caption='Uploaded Image', use_column_width=True)
+        # prediction
+        output = helper.prediction(frame)
+        str_out = 'prediction : ' + str(output[0])
+        st.success(str_out)
 # Image Input
 if selectbox == 'Upload Image':
     Uploaded_file = st.sidebar.file_uploader(
@@ -27,9 +41,8 @@ if selectbox == 'Upload Image':
         st.success(str_out)
 
 # CAMERA Input
-if selectbox == 'Live Camera':
+if selectbox == 'Live Camera (Experimental)':
     Start_camera = st.sidebar.checkbox("Start Camera")
-
     if Start_camera is True:
         st.title("Start Camera")
         FRAME_WINDOW = st.image([])
@@ -45,4 +58,7 @@ if selectbox == 'Live Camera':
                 str_out = 'prediction : ' + str(output[0])
                 FRAME_WINDOW1.success(str_out)
             else:
-                FRAME_WINDOW1.error("Camera not found")
+                FRAME_WINDOW.error("Streamlit as of now is NOT well Optimized for Live Camera support, I will update the application as soon as support rolls out")
+                FRAME_WINDOW1.error("Camera Not Found")
+    else:
+        st.sidebar.markdown("Please check the checkbox to start the camera")
